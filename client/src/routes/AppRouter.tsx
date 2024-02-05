97b6b3072756910dd1f76176e4dc38a1c5a842c9
elibes <elibes@proton.me>
2024-02-05 20:04:39 +0100

    optimize AppRouter performance

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -6,10 +6,9 @@ import {
   Routes,
   Navigate,
 } from 'react-router-dom';
-import LoginView from '../view/LoginView';
-import RegistrationView from '../view/RegistrationView';
-import NotFoundView from '../view/NotFoundView';
-
+const LoginView = React.lazy(() => import('../view/LoginView'));
+const RegistrationView = React.lazy(() => import('../view/RegistrationView'));
+const NotFoundView = React.lazy(() => import('../view/NotFoundView'));
 /**
  * AppRouter returns a Router component with three Route components, rendering different views based on the path.
  * @returns A Router component containing three Route components.
@@ -18,11 +17,31 @@ const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginView />} />
-        <Route path="/register" element={<RegistrationView />} />
-        <Route path="/" element={<Navigate to="/login" />} exact />
-        <Route path={'*'} element={<NotFoundView />} />
-        {/* Catch-all route */}
+        <Route
+          path="/login"
+          element={
+            <React.Suspense fallback={<div>Loading...</div>}>
+              <LoginView />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <React.Suspense fallback={<div>Loading...</div>}>
+              <RegistrationView />
+            </React.Suspense>
+          }
+        />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route
+          path={'*'}
+          element={
+            <React.Suspense fallback={<div>Loading...</div>}>
+              <NotFoundView />
+            </React.Suspense>
+          }
+        />
       </Routes>
     </Router>
   );
