935700b668b2db95a573c73558c74d17bddfa6f3
elibes <elibes@proton.me>
2024-02-05 18:11:16 +0100

    small updates on AppRouter and Login styling

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Navigate} from 'react-router-dom';
-import LoginView from './views/LoginView';
-import RegistrationView from './view/RegistrationView';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+import LoginView from '../view/LoginView';
+import RegistrationView from '../view/RegistrationView';
 
 /**
  * AppRouter returns a Router component with three Route components, rendering different views based on the path.
@@ -10,9 +15,11 @@ import RegistrationView from './view/RegistrationView';
 const AppRouter = () => {
   return (
     <Router>
-      <Route path="/login" element={<LoginView />} />
-      <Route path="/register" element={<RegistrationView />} />
-      <Route path="/" element={<Navigate to="/login" />} />
+      <Routes>
+        <Route path="/login" element={<LoginView />} />
+        <Route path="/register" element={<RegistrationView />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
     </Router>
   );
 };
--- client/src/view/components/LoginForm.tsx
+++ client/src/view/components/LoginForm.tsx
@@ -2,27 +2,32 @@ import React, {useState} from 'react';
 import InputField from './InputField';
 import Button from './Button';
 import useLoginViewModel from '../../viewmodel/useLoginViewModel';
+import '../styles/LoginForm.css';
 
 const LoginForm = () => {
   const {username, setUsername, password, setPassword, error, handleSubmit} =
     useLoginViewModel();
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="login-form">
       {error && <p>{error}</p>}
-      <InputField
-        label="Username"
-        type="text"
-        value={username}
-        onChange={e => setUsername(e.target.value)}
-      />
-      <InputField
-        label="Password"
-        type="password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      <Button text="Login" onClick={() => {}} />
+      <div className="form-group">
+        <InputField
+          label="Username"
+          type="text"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <InputField
+          label="Password"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+      <Button text="Login" onClick={() => {}} className="login-button" />
     </form>
   );
 };
