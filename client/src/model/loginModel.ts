773889be20b59e4b975aa6ccc6ffa9119410893f
Alexander Olegård <aolegard@kth.se>
2024-02-16 21:31:08 +0100

    prettier fixing, env bug fix and button bug fix

--- client/src/model/loginModel.ts
+++ client/src/model/loginModel.ts
@@ -1,13 +1,3 @@
-/**
- * Handles user registration by sending user details to the registration API endpoint.
- *
- * @async
- * @function loginModel
- * @param {string} userName - Chosen username.
- * @param {string} password - Account password.
- * @returns {Promise<any>} - The response from the registration process, which can be a success message or an error message.
- * @throws {Error} Throws an error if the registration process fails or if the fetch operation encounters an error.
- */
 async function loginModel(userName: string, password: string): Promise<any> {
   const url = 'http://localhost:3001/user/login'; // API endpoint
   const payload = {
@@ -26,11 +16,12 @@ async function loginModel(userName: string, password: string): Promise<any> {
       const errorText = await response.text();
       throw new Error(`HTTP error ${response.status}: ${errorText}`);
     }
-    */
+
+ */
     return await response.json();
   } catch (error) {
     console.error('Fetch operation failed:', error);
-    throw new Error('Login failed');
+    throw new Error('Registration failed');
   }
 }
 
