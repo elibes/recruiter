dafe7b3920aa0f8ed445a657014f3ed3155049c6
Alexander Olegård <aolegard@kth.se>
2024-02-18 17:09:00 +0100

    cleaned up application form

--- client/src/view/components/ApplicationForm.tsx
+++ client/src/view/components/ApplicationForm.tsx
@@ -14,24 +14,46 @@ import {
   submitApplication,
 } from '../../viewmodel/applicationSlice';
 import {AppDispatch, RootState} from '../../store';
+import * as React from 'react';
 
 const ApplicationForm: FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const ranges = useSelector(
     (state: RootState) => state.application.availability
   );
-
   const errors = useSelector((state: RootState) => state.application.errorList);
   const resultMsg = useSelector(
     (state: RootState) => state.application.resultMsg
   );
-
   const selectionList = ranges.map(range => {
     return {
       startDate: new Date(range.startDate),
       endDate: new Date(range.endDate),
     };
   });
+
+  const handleDateRangeSet = (ranges: any) => {
+    const rangeKey = Object.keys(ranges)[0];
+    const formattedRange = {
+      startDate: ranges[rangeKey].startDate.toISOString(),
+      endDate: ranges[rangeKey].endDate.toISOString(),
+      key: rangeKey,
+    };
+    dispatch(setDates(formattedRange));
+  };
+
+  const handleNewAvailabilityButton = () => {
+    dispatch(createNewAvailability());
+  };
+
+  const handleSubmitApplicationButton = () => {
+    dispatch(applicationValidator());
+    dispatch(submitApplication());
+  };
+
+  const handleCancelApplicationButton = () => {
+    dispatch(cancelApplication());
+  };
 
   return (
     <div>
@@ -41,28 +63,19 @@ const ApplicationForm: FC = () => {
         ranges={selectionList}
         minDate={new Date()}
         maxDate={addYears(new Date(), 1)}
-        onChange={ranges => {
-          const rangeKey = Object.keys(ranges)[0];
-          const formattedRange = {
-            startDate: ranges[rangeKey].startDate.toISOString(),
-            endDate: ranges[rangeKey].endDate.toISOString(),
-            key: rangeKey,
-          };
-          dispatch(setDates(formattedRange));
-        }}
+        onChange={handleDateRangeSet}
       ></DateRangePicker>
-      <button onClick={() => dispatch(createNewAvailability())}>+</button>
-      <button
-        onClick={() => {
-          dispatch(applicationValidator());
-          dispatch(submitApplication());
-        }}
-      >
-        Submit Application
+      <button onClick={handleNewAvailabilityButton}>
+        Add another availability
       </button>
-      <button onClick={() => dispatch(cancelApplication())}>
-        Cancel Application
-      </button>
+      <div>
+        <button onClick={handleSubmitApplicationButton}>
+          Submit Application
+        </button>
+        <button onClick={handleCancelApplicationButton}>
+          Cancel Application
+        </button>
+      </div>
       {errors ? <span>{errors}</span> : ''}
       <span>{resultMsg}</span>
     </div>
