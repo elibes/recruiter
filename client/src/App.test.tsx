bf212be8c75ac5116a35e9e8bdea06c4d5b9be71
elibes <elibes@proton.me>
2024-02-29 19:40:28 +0100

    dummy test for client side

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import {render, screen} from '@testing-library/react';
+import {act, render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
+test('renders learn react link', async () => {
+  await act(async () => {
+    render(<App />);
+  });
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
