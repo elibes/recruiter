665fe0efac763ea9717bad608bdb6507c78ad593
elibes <elibes@proton.me>
2024-03-01 13:34:12 +0100

    remove unused code

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,8 +1,3 @@
-import * as React from 'react';
-import {render, screen} from '@testing-library/react';
-import App from './App';
-import {act} from 'react-dom/test-utils';
-
 it('should pass the dummy test', () => {
   expect(true).toBe(true);
 });
